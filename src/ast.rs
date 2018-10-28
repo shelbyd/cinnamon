f20d28aff7d121b79cd14d5ed2dc35ca7cf8ec47
Shelby Doolittle <shelby@shelbyd.com>
2018-10-28 18:26:04 -0400

    use executor injected dependency for testing command

--- src/ast.rs
+++ src/ast.rs
@@ -2,6 +2,20 @@ use std::process::{Command as ProcessCommand, ExitStatus};
 
 use failure::*;
 
+struct StdExecutor;
+
+impl Executor for StdExecutor {
+    type ExitStatus = ExitStatus;
+
+    fn execute(&mut self, command: &str, args: &[&str]) -> Result<Self::ExitStatus, Error> {
+        let exit = ProcessCommand::new(command)
+            .args(args)
+            .spawn()?
+            .wait()?;
+        Ok(exit)
+    }
+}
+
 #[derive(PartialEq, Eq, Debug)]
 pub enum AST {
     Comment(String),
@@ -13,9 +27,10 @@ pub enum AST {
 
 impl AST {
     pub fn execute(&self) -> Result<Option<ExitStatus>, Error> {
+        let mut executor = StdExecutor;
         match self {
             AST::Comment(_) => Ok(None),
-            AST::Command(c) => c.execute().map(Some),
+            AST::Command(c) => c.execute(&mut executor).map(Some),
             AST::If(c) => c.execute(),
             AST::Block(b) => b.execute(),
             AST::While(w) => w.execute(),
@@ -42,11 +57,11 @@ impl Command {
 }
 
 impl Command {
-    fn execute(&self) -> Result<ExitStatus, Error> {
-        let exit = ProcessCommand::new(&self.command)
-            .args(&self.args)
-            .spawn()?
-            .wait()?;
+    fn execute<E: Executor>(&self, executor: &mut E) -> Result<E::ExitStatus, Error> {
+        let exit = executor.execute(
+            &self.command,
+            &self.args.iter().map(AsRef::as_ref).collect::<Vec<_>>(),
+        )?;
         Ok(exit)
     }
 }
@@ -68,7 +83,7 @@ impl Conditional {
     }
 
     fn execute(&self) -> Result<Option<ExitStatus>, Error> {
-        if self.predicate.execute()?.success() {
+        if self.predicate.execute(&mut StdExecutor)?.success() {
             self.if_block.execute()
         } else {
             match &self.else_block {
@@ -112,7 +127,7 @@ impl While {
 
     fn execute(&self) -> Result<Option<ExitStatus>, Error> {
         let mut last_exit = None;
-        while self.predicate.execute()?.success() {
+        while self.predicate.execute(&mut StdExecutor)?.success() {
             last_exit = self.block.execute()?.or(last_exit);
             if let Some(exit) = last_exit {
                 if !exit.success() {
@@ -121,5 +136,61 @@ impl While {
             }
         }
         Ok(last_exit)
+    }
+}
+
+trait Executor {
+    type ExitStatus;
+
+    fn execute(&mut self, command: &str, args: &[&str]) -> Result<Self::ExitStatus, Error>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestExecutor {
+        history: Vec<(String, Vec<String>)>,
+    }
+
+    impl TestExecutor {
+        fn new() -> TestExecutor {
+            TestExecutor {
+                history: Vec::new(),
+            }
+        }
+
+        fn last(&self) -> Option<(&str, Vec<&str>)> {
+            self.history
+                .last()
+                .map(|(s, a)| (s.as_ref(), a.iter().map(|s| s.as_ref()).collect()))
+        }
+    }
+
+    impl Executor for TestExecutor {
+        type ExitStatus = ();
+
+        fn execute(&mut self, command: &str, args: &[&str]) -> Result<Self::ExitStatus, Error> {
+            self.history.push((
+                command.to_string(),
+                args.iter().map(|s| s.to_string()).collect(),
+            ));
+            Ok(())
+        }
+    }
+
+    #[cfg(test)]
+    mod command {
+        use super::*;
+
+        #[test]
+        fn executes_using_the_provided_executor() {
+            let mut executor = TestExecutor::new();
+            let command = Command::new("foo", vec![]);
+
+            command.execute(&mut executor).unwrap();
+
+            assert_eq!(executor.last(), Some(("foo", vec![])));
+        }
     }
 }
